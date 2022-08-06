7b9e141c9e89008b0241725d70dfe60e03fe5597
Italo Rodrigues <italorodriguesxxii@gmail.com>
2022-08-06 20:27:17 -0300

    Aula 1.2 Styled component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { Button } from "./components/Button";
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <>
+    <Button variant="primary" />
+    <Button variant="secondary" />
+    </>
   )
 }
